4675b53a0dac79f804c0d23c40ad27664cef614b
YoungKwon Jo <Young-Kwon.Jo@cern.ch>
2015-02-12 22:59:30 +0900

    add list for plotting

--- CATTools/plot.C
+++ CATTools/plot.C
@@ -1,16 +1,85 @@
 
 void plot()
 {
-  plot2("gentop_NJets20","# of Jet in GEN","Entries",15,0,15,true,0);
-  plot2("nJet30","# of Jet30","Entries",10,0,10,true,4);
-  plot2("nbJet30T","# of bTag CSVT","Entries",5,0,5,true,5);
-  plot2("MET","MET","Entries",30,0,150,true,3);
-  plot2("ZMass","invariant mass of dilepton","Entries",50,0,200,true,1);
-  plot2("ZMass","invariant mass of dilepton","Entries",50,0,200,true,2);
-  plot2("lep1_pt","p_T of leading lepton","Entries",50,0,200,true,1);
-  plot2("lep2_pt","p_T of second leading lepton","Entries",50,0,200,true,1);
-  plot2("lep1_eta","#eta of leading lepton","Entries",50,-2.5,2.5,true,1);
-  plot2("lep2_eta","#eta of second leading lepton","Entries",50,-2.5,2.5,true,1);
+  bool log = true;
+  //plot2("gentop_NJets20","# of Jet in GEN","Entries",15,0,15,log,0);
+  //plot2("nJet30","# of Jet30","Entries",10,0,10,log,4);
+  //plot2("nbJet30T","# of bTag CSVT","Entries",5,0,5,log,5);
+  //plot2("MET","MET","Entries",30,0,150,log,3);
+  //plot2("ZMass","invariant mass of dilepton","Entries",50,0,200,log,2);
+
+  //S-1
+  plot2("gentop_NJets20","# of Jet in GEN","Entries",15,0,15,log,-1);
+
+ break;
+  //S0
+  plot2("lep1_pt","p_T of leading lepton","Entries",50,0,200,log,0);
+  plot2("lep2_pt","p_T of second leading lepton","Entries",50,0,200,log,0);
+  plot2("lep1_eta","#eta of leading lepton","Entries",50,-2.5,2.5,log,0);
+  plot2("lep2_eta","#eta of second leading lepton","Entries",50,-2.5,2.5,log,0);
+  plot2("lep1_relIso","relIso of leading lepton","Entries",50,0,200,log,0);
+  plot2("lep2_relIso","relIso of second leading lepton","Entries",50,0,200,log,0);
+ 
+  //S1
+  plot2("ZMass","invariant mass of dilepton","Entries",50,0,200,log,1);
+  plot2("lep1_pt","p_T of leading lepton","Entries",50,0,200,log,1);
+  plot2("lep2_pt","p_T of second leading lepton","Entries",50,0,200,log,1);
+  plot2("lep1_eta","#eta of leading lepton","Entries",50,-2.5,2.5,log,1);
+  plot2("lep2_eta","#eta of second leading lepton","Entries",50,-2.5,2.5,log,1);
+
+  //S2
+  plot2("ZMass","invariant mass of dilepton","Entries",50,0,200,log,2);
+  plot2("MET","MET","Entries",30,0,150,log,2);
+
+  //S3
+  plot2("MET","MET","Entries",30,0,150,log,3);
+  plot2("nJet30","# of Jet30","Entries",10,0,10,log,3);
+
+  //S4
+  plot2("nJet30","# of Jet30","Entries",10,0,10,log,4);
+  plot2("nbJet30T","# of bTag CSVT","Entries",5,0,5,log,4);
+  plot2("nbJet30M","# of bTag CSVM","Entries",7,0,7,log,4);
+  plot2("nbJet30L","# of bTag CSVL","Entries",7,0,7,log,4);
+
+  plot2("jet1_pt","p_T of 1st jet","Entries",50,0,200,log,4);
+  plot2("jet1_eta","#eta of 1st jet","Entries",25,-2.5,2.5,log,4);
+  plot2("jet1_phi","#phi of 1st jet","Entries",25,-2.5,2.5,log,4);
+  plot2("jet1_bTag","CSV of 1st jet","Entries",10,0,10,log,4);
+
+  plot2("jet2_pt","p_T of 2nd jet","Entries",50,0,200,log,4);
+  plot2("jet2_eta","#eta of 2nd jet","Entries",25,-2.5,2.5,log,4);
+  plot2("jet2_phi","#phi of 2nd jet","Entries",25,-2.5,2.5,log,4);
+  plot2("jet2_bTag","CSV of 2nd jet","Entries",10,0,10,log,4);
+
+  plot2("jet3_pt","p_T of 3rd jet","Entries",50,0,200,log,4);
+  plot2("jet3_eta","#eta of 3rd jet","Entries",25,-2.5,2.5,log,4);
+  plot2("jet3_phi","#phi of 3rd jet","Entries",25,-2.5,2.5,log,4);
+  plot2("jet3_bTag","CSV of 3rd jet","Entries",10,0,10,log,4);
+
+  plot2("jet4_pt","p_T of 4th jet","Entries",50,0,200,log,4);
+  plot2("jet4_eta","#eta of 4th jet","Entries",25,-2.5,2.5,log,4);
+  plot2("jet4_phi","#phi of 4th jet","Entries",25,-2.5,2.5,log,4);
+  plot2("jet4_bTag","CSV of 4th jet","Entries",10,0,10,log,4);
+
+  //S51
+  plot2("jet1_bTag","CSV of 1st jet","Entries",10,0,10,log,5);
+  plot2("jet2_bTag","CSV of 2nd jet","Entries",10,0,10,log,5);
+  plot2("jet3_bTag","CSV of 3rd jet","Entries",10,0,10,log,5);
+  plot2("jet4_bTag","CSV of 4th jet","Entries",10,0,10,log,5);
+
+   //S52
+  plot2("jet1_bTag","CSV of 1st jet","Entries",10,0,10,log,6);
+  plot2("jet2_bTag","CSV of 2nd jet","Entries",10,0,10,log,6);
+  plot2("jet3_bTag","CSV of 3rd jet","Entries",10,0,10,log,6);
+  plot2("jet4_bTag","CSV of 4th jet","Entries",10,0,10,log,6);
+
+  //S53
+  plot2("jet1_bTag","CSV of 1st jet","Entries",10,0,10,log,7);
+  plot2("jet2_bTag","CSV of 2nd jet","Entries",10,0,10,log,7);
+  plot2("jet3_bTag","CSV of 3rd jet","Entries",10,0,10,log,7);
+  plot2("jet4_bTag","CSV of 4th jet","Entries",10,0,10,log,7);
+
+
 
 }
 void plot2(char *plotname, char* xtitle, char* ytitle, int nBins, double min, double max, bool logy, int cutN) 
@@ -26,19 +95,25 @@ void plot2(char *plotname, char* xtitle, char* ytitle, int nBins, double min, do
   //char *plotname = "lep2_eta", *xtitle = "#eta of second leading lepton ", *ytitle = "Entries"; int nBins = 50; double min =-2.5, max=2.5; bool logy = true;
 
 ///////////////////////
-  TCut S1 = "lep1_pt>20. && abs(lep1_eta)<2.5 && lep2_pt>20. && abs(lep2_eta)<2.5 && lep1_relIso<0.15 &&  lep2_relIso<0.15 && ZMass>12 && lep1_Q*lep2_Q<0 ";
+  TCut S0 = "lep1_pt>20. && abs(lep1_eta)<2.5 && lep2_pt>20. && abs(lep2_eta)<2.5";
+  TCut S1 = "lep1_relIso<0.15 &&  lep2_relIso<0.15 && ZMass>12 && lep1_Q*lep2_Q<0 ";
   TCut S2 = "abs(ZMass-91)>15";
   TCut S3 = "MET>30";
   TCut S4 = "nJet30>=4";
-  TCut S5 = "nbJet30T>=2";
+  TCut S51 = "nbJet30L>=2";
+  TCut S52 = "nbJet30M>=2";
+  TCut S53 = "nbJet30T>=2";
 
 
   TCut stepcut = "1";  char *step = "nocut";
-  if(cutN==1) {  stepcut = S1;  step = "S1"; }
-  if(cutN==2) { stepcut = S1+S2;  step = "S2"; }
-  if(cutN==3) { stepcut = S1+S2+S3;  step = "S3"; }
-  if(cutN==4) { stepcut = S1+S2+S3+S4;  step = "S4"; }
-  if(cutN==5) { stepcut = S1+S2+S3+S4+S5;  step = "S5"; }
+  if(cutN==0) { stepcut = S0;  step = "S0"; }
+  if(cutN==1) { stepcut = S0+S1;  step = "S1"; }
+  if(cutN==2) { stepcut = S0+S1+S2;  step = "S2"; }
+  if(cutN==3) { stepcut = S0+S1+S2+S3;  step = "S3"; }
+  if(cutN==4) { stepcut = S0+S1+S2+S3+S4;  step = "S4"; }
+  if(cutN==5) { stepcut = S0+S1+S2+S3+S4+S51;  step = "S51"; }
+  if(cutN==6) { stepcut = S0+S1+S2+S3+S4+S52;  step = "S52"; }
+  if(cutN==7) { stepcut = S0+S1+S2+S3+S4+S53;  step = "S53"; }
 
 //////////////////////////////////////
   gStyle->SetOptStat(0); //remove statistics box
@@ -52,7 +127,14 @@ void plot2(char *plotname, char* xtitle, char* ytitle, int nBins, double min, do
 
   TChain * chain = new TChain("ntupleMuMu");
   chain->Add("mini_1.root");
-  //chain->Add("file2.root");
+  chain->Add("mini_2.root");
+  chain->Add("mini_3.root");
+  chain->Add("mini_4.root");
+  chain->Add("mini_5.root");
+  chain->Add("mini_6.root");
+  chain->Add("mini_7.root");
+  chain->Add("mini_8.root");
+  chain->Add("mini_9.root");
   TTree * atree = chain;
 
   TCut visible = "(gentop_NJets20>=4)&&(gentop_NbJets20>=2)&&(gentop_lep1_pt>20)&&(abs(gentop_lep1_eta)<2.4)&&(gentop_lep2_pt>20)&&(abs(gentop_lep2_eta)<2.4)";
@@ -76,8 +158,8 @@ void plot2(char *plotname, char* xtitle, char* ytitle, int nBins, double min, do
   for(int i=0;i<5;i++)
   {
       //h1[i] = new TH1F(Form("h_%s",mcname[i]),Form("%s of %s",plotname,mcsample[i]),nBins,min,max);
-      h1[i] = new TH1F(Form("h_%s",mcname[i]),Form("%s ",plotname),nBins,min,max);
-      chain->Project(Form("h_%s",mcname[i]),plotname,cut[i]+stepcut);
+      h1[i] = new TH1F(Form("h_%s_%s_%s",mcname[i],plotname,step),Form("%s ",plotname),nBins,min,max);
+      chain->Project(Form("h_%s_%s_%s",mcname[i],plotname,step),plotname,cut[i]+stepcut);
       h1[i]->AddBinContent(nBins, h1[i]->GetBinContent(nBins+1));
       h1[i]->SetLineColor(color[i]);      
       h1[i]->GetXaxis()->SetTitle(xtitle);
@@ -126,5 +208,5 @@ void plot2(char *plotname, char* xtitle, char* ytitle, int nBins, double min, do
   pt->AddText(Form("ttbb/ttjj = %.5f",nume/denom));
   pt->Draw();
 
-  c1->Print(Form("plots/%s_%s.eps",plotname,step));
+  c1->Print(Form("plots/MuMu_%s_%s.eps",plotname,step));
 }
