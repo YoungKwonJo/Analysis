9dadd626ebe57be470f877a43a71869c4a51c8cb
YoungKwonJo <Young-Kwon.Jo@cern.ch>
2015-03-27 01:41:09 +0900

    update ploterNew

--- Delphes/plot.py
+++ Delphes/plot.py
@@ -5,22 +5,42 @@ import sys,os
 
 # root -l ploterNew.C+\(0,0,true\)
 
-for i in range(0,22) :
-  """
-  for j in range(0,5) :
+for i in range(1,45) :
+#  """
+  for j in range(1,6) :
     cmd = "root -b -q ploterNew.C+\\(%d"% i
+    cmd = cmd+",%d"% j
+    cmd = cmd+",true\\)"
+    print cmd
+    os.system(cmd)
+#  """ 
+
+for i in range(1,45) :
+  """
+  for j in range(1,6) :
+    cmd = "root -b -q ploterNew.C+\\(%d"% i
+    cmd = cmd+",%d"% j
+    cmd = cmd+",false\\)"
+    print cmd
+    os.system(cmd)
+  """
+
+for i in range(1,44) :
+  """
+  for j in range(1,6) :
+    cmd = "root -b -q ploterNew_lj.C+\\(%d"% i
     cmd = cmd+",%d"% j
     cmd = cmd+",true\\)"
     print cmd
     os.system(cmd)
   """ 
 
-for i in range(0,22) :
-#  """
-  for j in range(0,5) :
-    cmd = "root -b -q ploterNew.C+\\(%d"% i
+for i in range(1,44) :
+  """
+  for j in range(1,6) :
+    cmd = "root -b -q ploterNew_lj.C+\\(%d"% i
     cmd = cmd+",%d"% j
     cmd = cmd+",false\\)"
     print cmd
     os.system(cmd)
-#  """
+  """
