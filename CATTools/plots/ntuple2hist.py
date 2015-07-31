915a358b274321bdfdcb84f377a8d9ab2d0e1a7f
YoungKwonJo <Young-Kwon.Jo@cern.ch>
2015-07-31 11:31:57 +0900

    update it

--- CATTools/plots/ntuple2hist.py
+++ CATTools/plots/ntuple2hist.py
@@ -16,6 +16,13 @@ ee_el2 = "((ee_el2_pt>20) && (abs(ee_el2_eta)<2.4) && (abs(ee_el2_iso)<0.12))"
 em_mu1 = "((em_mu1_pt>20) && (abs(em_mu1_eta)<2.4) && (abs(em_mu1_iso)<0.12))"
 em_el2 = "((em_el2_pt>20) && (abs(em_el2_eta)<2.4) && (abs(em_el2_iso)<0.12))"
 
+mm_lep = "(" + mm_mu1 + " && "+mm_mu2 + " && (mm_zmass>20))"
+ee_lep = "(" + ee_el1 + " && "+ee_el2 + " && (ee_zmass>20))"
+em_lep = "(" + em_mu1 + " && "+em_el2 + " && (em_zmass>20))"
+
+mm_zmass = "( abs(91.2-mm_zmass)>15 )"
+ee_zmass = "( abs(91.2-ee_zmass)>15 )"
+em_zmass = "( abs(91.2-em_zmass)>15 )"
 
 json = {
 "mcsamples" : mcsamples,
@@ -26,8 +33,9 @@ json = {
 #   "1",
 #   "(isMM ==1 || isEE==1 || isEM==1)",
 #   "((mm_zmass>20 && isMM==1) || (ee_zmass>20 && isEE==1)||(em_zmass>20 && isEM==1))",
-   "((" + mm_mu1 + " && "+mm_mu2 + " ) ||  (" + ee_el1 + " && "+ee_el2 + " ) ||  (" + em_mu1 + " && "+em_el2 + " )) && "
-   "(met>40 || (isEM==1)) && "
+   "(" + mm_lep + " || " + ee_lep + " || " + em_lep + ") && "
+   "(" + mAND(mm_lep,mm_zmass) + " || " + mAND(ee_lep,ee_zmass) + " || " + mAND(em_lep,em_zmass) + ") && "
+   "(met>40 || !(" + mm_lep + " || " + ee_lep + ")) && "
    "nJet>=4 ",
    "nBJetM>=2", 
    "nBJetT>=2", 
