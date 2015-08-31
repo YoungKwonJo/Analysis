9c8375a04015ad2ef4a82269b825475f10e53853
YoungKwonJo <Young-Kwon.Jo@cern.ch>
2015-08-31 23:57:57 +0900

    update the dilepton selection by only pt order in muon and electron

--- CATTools/plots/monitors_cfi.py
+++ CATTools/plots/monitors_cfi.py
@@ -1,57 +1,65 @@
 
 monitors = {
 "Mon1" : [
-  #in the figure MET_S0, we will use the event for total-event.
-  { "name":"MET",          "unit":"Missing E_{T} (GeV) ",          "var":"met",      "xbin_set":[60,0,300]   },
-  { "name":"ZMassMM",      "unit":"Dilepton mass (GeV/c^{2}) ",    "var":"mm_zmass",  "xbin_set":[60,0,300]  },
-  { "name":"nGoodPV",      "unit":"# of good vertex ",             "var":"nGoodPV",  "xbin_set":[70,0,70]    },
-  { "name":"nPV",          "unit":"# of vertex ",                  "var":"nPV",      "xbin_set":[70,0,70]    },
-  { "name":"ZMassEE",      "unit":"Dilepton mass (GeV/c^{2}) ",    "var":"ee_zmass",  "xbin_set":[60,0,300]  },
-  { "name":"ZMassEM",      "unit":"Dilepton mass (GeV/c^{2}) ",    "var":"em_zmass",  "xbin_set":[60,0,300]  },
+  { "name":"MET",          "unit":"Missing E_{T} without HF (GeV)","var":"metNoHF",   "xbin_set":[60,0,300]  },
+#  { "name":"ZMassMM",      "unit":"Dilepton mass (GeV/c^{2}) ",    "var":"mm_zmass",  "xbin_set":[60,0,300]  },
+  { "name":"ZMass",        "unit":"Dilepton mass (GeV/c^{2}) ",    "var":"ll_zmass",  "xbin_set":[60,0,300]  },
+  { "name":"nGoodPV",      "unit":"# of good vertex ",             "var":"nGoodPV",   "xbin_set":[70,0,70]    },
+  { "name":"nPV",          "unit":"# of vertex ",                  "var":"nPV",       "xbin_set":[70,0,70]    },
+#  { "name":"ZMassEE",      "unit":"Dilepton mass (GeV/c^{2}) ",    "var":"ee_zmass",  "xbin_set":[60,0,300]  },
+#  { "name":"ZMassEM",      "unit":"Dilepton mass (GeV/c^{2}) ",    "var":"em_zmass",  "xbin_set":[60,0,300]  },
 ],
 "Mon2" : [
-  { "name":"Stat",         "unit":"Stat ",                         "var":"met",      "xbin_set":[1,0,10000]     },
-  { "name":"METPHI",       "unit":"Missing E_{T} #phi ",           "var":"metphi",   "xbin_set":[40,-4,4]      },
-  { "name":"nBJetL",       "unit":"b-Jet multiplicity loose ",     "var":"nBJetL",   "xbin_set":[8,0,8]      },
-  { "name":"nBJetM",       "unit":"b-Jet multiplicity medium ",    "var":"nBJetM",   "xbin_set":[8,0,8]      },
-  { "name":"NJet",         "unit":"Jet multiplicity ",            "var":"nJet",         "xbin_set":[10,0,10]    },
-  { "name":"nBJetT",       "unit":"b-Jet multiplicity tight ",    "var":"nBJetT",       "xbin_set":[8,0,8]      },
+  { "name":"Stat",         "unit":"Stat ",                         "var":"met",       "xbin_set":[1,0,10000]  },
+  { "name":"METPHI",       "unit":"Missing E_{T} without HF #phi", "var":"metNoHFphi","xbin_set":[40,-4,4]    },
+  { "name":"nBJetL",       "unit":"b-Jet multiplicity loose ",     "var":"nBJetL",    "xbin_set":[8,0,8]      },
+  { "name":"nBJetM",       "unit":"b-Jet multiplicity medium ",    "var":"nBJetM",    "xbin_set":[8,0,8]      },
+  { "name":"NJet",         "unit":"Jet multiplicity ",             "var":"nJet",      "xbin_set":[10,0,10]    },
+  { "name":"nBJetT",       "unit":"b-Jet multiplicity tight ",     "var":"nBJetT",    "xbin_set":[8,0,8]      },
 ],
 "Mon3" : [
-  { "name":"lep1PtMM",     "unit":"Leading lepton p_{T} (GeV/c) ", "var":"mm_mu1_pt", "xbin_set":[40,0,200] },
-  { "name":"lep1PtEE",     "unit":"Leading lepton p_{T} (GeV/c) ", "var":"ee_el1_pt", "xbin_set":[40,0,200] },
-  { "name":"lep1PtEM",     "unit":"Leading lepton p_{T} (GeV/c) ", "var":"em_mu1_pt", "xbin_set":[40,0,200] },
+  { "name":"lep1Pt",       "unit":"Leading lepton p_{T} (GeV/c) ", "var":"ll_lep1_pt", "xbin_set":[40,0,200] },
+#  { "name":"lep1PtMM",     "unit":"Leading lepton p_{T} (GeV/c) ", "var":"mm_mu1_pt", "xbin_set":[40,0,200] },
+#  { "name":"lep1PtEE",     "unit":"Leading lepton p_{T} (GeV/c) ", "var":"ee_el1_pt", "xbin_set":[40,0,200] },
+#  { "name":"lep1PtEM",     "unit":"Leading lepton p_{T} (GeV/c) ", "var":"em_mu1_pt", "xbin_set":[40,0,200] },
 
-  { "name":"lep2PtMM",     "unit":"Sceond  lepton p_{T} (GeV/c) ", "var":"mm_mu2_pt", "xbin_set":[40,0,200] },
-  { "name":"lep2PtEE",     "unit":"Sceond  lepton p_{T} (GeV/c) ", "var":"ee_el2_pt", "xbin_set":[40,0,200] },
-  { "name":"lep2PtEM",     "unit":"Sceond  lepton p_{T} (GeV/c) ", "var":"em_el2_pt", "xbin_set":[40,0,200] },
+  { "name":"lep2Pt",       "unit":"Sceond  lepton p_{T} (GeV/c) ", "var":"ll_lep2_pt", "xbin_set":[40,0,200] },
+#  { "name":"lep2PtMM",     "unit":"Sceond  lepton p_{T} (GeV/c) ", "var":"mm_mu2_pt", "xbin_set":[40,0,200] },
+#  { "name":"lep2PtEE",     "unit":"Sceond  lepton p_{T} (GeV/c) ", "var":"ee_el2_pt", "xbin_set":[40,0,200] },
+#  { "name":"lep2PtEM",     "unit":"Sceond  lepton p_{T} (GeV/c) ", "var":"em_el2_pt", "xbin_set":[40,0,200] },
 ],
 "Mon4" : [
-  { "name":"lep1EtaMM",    "unit":"Leading lepton #eta ",          "var":"mm_mu1_eta", "xbin_set":[30,-3,3] },
-  { "name":"lep1EtaEE",    "unit":"Leading lepton #eta ",          "var":"ee_el1_eta", "xbin_set":[30,-3,3] },
-  { "name":"lep1EtaEM",    "unit":"Leading lepton #eta ",          "var":"em_mu1_eta", "xbin_set":[30,-3,3] },
+  { "name":"lep1Eta",      "unit":"Leading lepton #eta ",          "var":"ll_lep1_eta", "xbin_set":[30,-3,3] },
+#  { "name":"lep1EtaMM",    "unit":"Leading lepton #eta ",          "var":"mm_mu1_eta", "xbin_set":[30,-3,3] },
+#  { "name":"lep1EtaEE",    "unit":"Leading lepton #eta ",          "var":"ee_el1_eta", "xbin_set":[30,-3,3] },
+#  { "name":"lep1EtaEM",    "unit":"Leading lepton #eta ",          "var":"em_mu1_eta", "xbin_set":[30,-3,3] },
 
-  { "name":"lep2EtaMM",    "unit":"Sceond  lepton #eta ",          "var":"mm_mu2_eta", "xbin_set":[30,-3,3] },
-  { "name":"lep2EtaEE",    "unit":"Sceond  lepton #eta ",          "var":"ee_el2_eta", "xbin_set":[30,-3,3] },
-  { "name":"lep2EtaEM",    "unit":"Sceond  lepton #eta ",          "var":"em_el2_eta", "xbin_set":[30,-3,3] },
+  { "name":"lep2Eta",    "unit":"Sceond  lepton #eta ",          "var":"ll_lep2_eta", "xbin_set":[30,-3,3] },
+#  { "name":"lep2EtaMM",    "unit":"Sceond  lepton #eta ",          "var":"mm_mu2_eta", "xbin_set":[30,-3,3] },
+#  { "name":"lep2EtaEE",    "unit":"Sceond  lepton #eta ",          "var":"ee_el2_eta", "xbin_set":[30,-3,3] },
+#  { "name":"lep2EtaEM",    "unit":"Sceond  lepton #eta ",          "var":"em_el2_eta", "xbin_set":[30,-3,3] },
 ],
 "Mon5" : [
-  { "name":"lep1PhiMM",    "unit":"Leading lepton #phi ",          "var":"mm_mu1_phi", "xbin_set":[40,-4,4] },
-  { "name":"lep1PhiEE",    "unit":"Leading lepton #phi ",          "var":"ee_el1_phi", "xbin_set":[40,-4,4] },
-  { "name":"lep1PhiEM",    "unit":"Leading lepton #phi ",          "var":"em_mu1_phi", "xbin_set":[40,-4,4] },
+  { "name":"lep1Phi",      "unit":"Leading lepton #phi ",          "var":"ll_lep1_phi", "xbin_set":[40,-4,4] },
+#  { "name":"lep1PhiMM",    "unit":"Leading lepton #phi ",          "var":"mm_mu1_phi", "xbin_set":[40,-4,4] },
+#  { "name":"lep1PhiEE",    "unit":"Leading lepton #phi ",          "var":"ee_el1_phi", "xbin_set":[40,-4,4] },
+#  { "name":"lep1PhiEM",    "unit":"Leading lepton #phi ",          "var":"em_mu1_phi", "xbin_set":[40,-4,4] },
 
-  { "name":"lep2PhiMM",    "unit":"Sceond  lepton #phi ",          "var":"mm_mu2_phi", "xbin_set":[40,-4,4] },
-  { "name":"lep2PhiEE",    "unit":"Sceond  lepton #phi ",          "var":"ee_el2_phi", "xbin_set":[40,-4,4] },
-  { "name":"lep2PhiEM",    "unit":"Sceond  lepton #phi ",          "var":"em_el2_phi", "xbin_set":[40,-4,4] },
+  { "name":"lep2Phi",      "unit":"Sceond  lepton #phi ",          "var":"ll_lep2_phi", "xbin_set":[40,-4,4] },
+#  { "name":"lep2PhiMM",    "unit":"Sceond  lepton #phi ",          "var":"mm_mu2_phi", "xbin_set":[40,-4,4] },
+#  { "name":"lep2PhiEE",    "unit":"Sceond  lepton #phi ",          "var":"ee_el2_phi", "xbin_set":[40,-4,4] },
+#  { "name":"lep2PhiEM",    "unit":"Sceond  lepton #phi ",          "var":"em_el2_phi", "xbin_set":[40,-4,4] },
 ],
 "Mon6" : [
-  { "name":"lep1IsoMM",    "unit":"Leading lepton Iso_{rel} ",     "var":"mm_mu1_iso", "xbin_set":[20,0,1] },
-  { "name":"lep1IsoEE",    "unit":"Leading lepton Iso_{rel} ",     "var":"ee_el1_iso", "xbin_set":[20,0,1] },
-  { "name":"lep1IsoEM",    "unit":"Leading lepton Iso_{rel} ",     "var":"em_mu1_iso", "xbin_set":[20,0,1] },
+  { "name":"lep1Iso",      "unit":"Leading lepton Iso_{rel} ",     "var":"ll_lep1_iso", "xbin_set":[20,0,1] },
+#  { "name":"lep1IsoMM",    "unit":"Leading lepton Iso_{rel} ",     "var":"mm_mu1_iso", "xbin_set":[20,0,1] },
+#  { "name":"lep1IsoEE",    "unit":"Leading lepton Iso_{rel} ",     "var":"ee_el1_iso", "xbin_set":[20,0,1] },
+#  { "name":"lep1IsoEM",    "unit":"Leading lepton Iso_{rel} ",     "var":"em_mu1_iso", "xbin_set":[20,0,1] },
 
-  { "name":"lep2IsoMM",    "unit":"Sceond  lepton Iso_{rel} ",     "var":"mm_mu2_iso", "xbin_set":[20,0,1] },
-  { "name":"lep2IsoEE",    "unit":"Sceond  lepton Iso_{rel} ",     "var":"ee_el2_iso", "xbin_set":[20,0,1] },
-  { "name":"lep2IsoEM",    "unit":"Sceond  lepton Iso_{rel} ",     "var":"em_el2_iso", "xbin_set":[20,0,1] },
+  { "name":"lep2Iso",      "unit":"Sceond  lepton Iso_{rel} ",     "var":"ll_lep2_iso", "xbin_set":[20,0,1] },
+#  { "name":"lep2IsoMM",    "unit":"Sceond  lepton Iso_{rel} ",     "var":"mm_mu2_iso", "xbin_set":[20,0,1] },
+#  { "name":"lep2IsoEE",    "unit":"Sceond  lepton Iso_{rel} ",     "var":"ee_el2_iso", "xbin_set":[20,0,1] },
+#  { "name":"lep2IsoEM",    "unit":"Sceond  lepton Iso_{rel} ",     "var":"em_el2_iso", "xbin_set":[20,0,1] },
 ],
 "Mon7" : [
   { "name":"jet1Pt",      "unit":"p_T of 1st leading Jet ",      "var":"jet_pt[0]",    "xbin_set":[40,0,400]   },
@@ -84,14 +92,18 @@ monitors = {
   { "name":"jet4DRl",     "unit":"DR_l of 4th leading Jet ",     "var":"jet_drl[3]",   "xbin_set":[20,0,4]     },
 ],
 "Mon13": [
- # { "name":"lep1DRelMM",   "unit":"Leading lepton DR with electron ",  "var":"mm_mu1_dr_el", "xbin_set":[60,0,3] },
- # { "name":"lep1DRmuEE",   "unit":"Leading lepton DR with muon     ",  "var":"ee_el1_dr_mu", "xbin_set":[60,0,3] },
- # { "name":"lep1DRelEM",   "unit":"Leading lepton DR with electron ",  "var":"em_mu1_dr_el", "xbin_set":[60,0,3] },
- # { "name":"lep2DRelMM",   "unit":"Second lepton DR with electron  ",  "var":"mm_mu2_dr_el", "xbin_set":[60,0,3] },
- # { "name":"lep2DRmuEE",   "unit":"Second lepton DR with muon      ",  "var":"ee_el2_dr_mu", "xbin_set":[60,0,3] },
- # { "name":"lep2DRmuEM",   "unit":"Second lepton DR with muon      ",  "var":"em_el2_dr_mu", "xbin_set":[60,0,3] },
+  { "name":"jetPP1CSV",   "unit":"CSV2 of 1st leading JetPuppi ",     "var":"jetPuppi_csv[0]",   "xbin_set":[20,0,1]     },
+  { "name":"jetPP2CSV",   "unit":"CSV2 of 2nd leading JetPuppi ",     "var":"jetPuppi_csv[1]",   "xbin_set":[20,0,1]     },
+  { "name":"jetPP3CSV",   "unit":"CSV2 of 3rd leading JetPuppi ",     "var":"jetPuppi_csv[2]",   "xbin_set":[20,0,1]     },
+  { "name":"jetPP4CSV",   "unit":"CSV2 of 4th leading JetPuppi ",     "var":"jetPuppi_csv[3]",   "xbin_set":[20,0,1]     },
 ],
-"Mon12" : [
+"Mon14" : [
+  { "name":"jetPP1Pt",      "unit":"p_T of 1st leading JetPuppi ",      "var":"jetPuppi_pt[0]",    "xbin_set":[40,0,400]   },
+  { "name":"jetPP2Pt",      "unit":"p_T of 2nd leading JetPuppi ",      "var":"jetPuppi_pt[1]",    "xbin_set":[40,0,400]   },
+  { "name":"jetPP3Pt",      "unit":"p_T of 3rd leading JetPuppi ",      "var":"jetPuppi_pt[2]",    "xbin_set":[40,0,400]   },
+  { "name":"jetPP4Pt",      "unit":"p_T of 4th leading JetPuppi ",      "var":"jetPuppi_pt[3]",    "xbin_set":[40,0,400]   },
+],
+"Mon15" : [
   { "name":"Nmu",         "unit":"number of muon ",              "var":"Nmu",          "xbin_set":[5,0,5]    },
   { "name":"NmuIso",      "unit":"number of muon ",              "var":"NmuIso",       "xbin_set":[5,0,5]    },
   { "name":"Nel",         "unit":"number of electron ",          "var":"Nel",          "xbin_set":[5,0,5]    },
@@ -100,26 +112,26 @@ monitors = {
 }
 
 monitors2d = {
-"Mon14" : [
+"Mon16" : [
   { "name":"jet1CSV",     "unit":"CSV2 of 1st leading Jet ",     "var":"jet_csv[0]",   "xbin_set":[20,0,1]     },
   { "name":"jet2CSV",     "unit":"CSV2 of 2nd leading Jet ",     "var":"jet_csv[1]",   "xbin_set":[20,0,1]     },
 ],
-"Mon15" : [
+"Mon17" : [
   { "name":"jet3CSV",     "unit":"CSV2 of 3rd leading Jet ",     "var":"jet_csv[2]",   "xbin_set":[20,0,1]     },
   { "name":"jet4CSV",     "unit":"CSV2 of 4th leading Jet ",     "var":"jet_csv[3]",   "xbin_set":[20,0,1]     },
 ],
-"Mon16" : [
+"Mon18" : [
   { "name":"NJet",        "unit":"Jet multiplicity ",             "var":"nJet",     "xbin_set":[10,0,10]    },
-  { "name":"nBJetL",      "unit":"b-Jet multiplicity loose ",     "var":"nBJetL",   "xbin_set":[8,0,8]      },
+#  { "name":"nBJetL",      "unit":"b-Jet multiplicity loose ",     "var":"nBJetL",   "xbin_set":[8,0,8]      },
   { "name":"nBJetM",      "unit":"b-Jet multiplicity medium ",    "var":"nBJetM",   "xbin_set":[8,0,8]      },
   { "name":"nBJetT",      "unit":"b-Jet multiplicity tight ",     "var":"nBJetT",   "xbin_set":[8,0,8]      },
 ],
-"Mon17" : [
+"Mon19" : [
   { "name":"NJet",        "unit":"Jet multiplicity ",            "var":"nJet",         "xbin_set":[10,0,10]    },
   { "name":"genNJet",     "unit":"genJet multiplicity ",         "var":"NgenJet",      "xbin_set":[10,0,10]    },
   { "name":"Category",    "unit":"Category ",                    "var":"Category",     "xbin_set":[5,0,5]      },
 ],
-"Mon18" : [
+"Mon20" : [
 #  { "name":"Nmu",         "unit":"number of muon ",              "var":"Nmu",          "xbin_set":[5,0,5]    },
   { "name":"NmuIso",      "unit":"number of muon ",              "var":"NmuIso",       "xbin_set":[5,0,5]    },
 #  { "name":"Nel",         "unit":"number of electron ",          "var":"Nel",          "xbin_set":[5,0,5]    },
